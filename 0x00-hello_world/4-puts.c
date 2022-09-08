731982ee11adf67676eb09f9c7ac165436c72307
ochochejay <ochochejay@gmail.com>
2022-09-08 12:31:47 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+ * Description : main - Prints a string to stdout.
+ * Returns 0 if successful
+ */
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }	
