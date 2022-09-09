7d787f459e97f169c9bab2ae3b31b00ca92c624b
Ochochejay <ochochejay@gmail.com>
2022-09-09 14:43:21 +0100

    lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lower and upper case alphabets to stdout.
+ * Return: 0 if successful.
+*/
+
+int main(void)
+{
+	char lower;
+	char upper;
+
+	lower = 'a';
+	upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
