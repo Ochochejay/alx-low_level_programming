8d83d5e1867850c862d525fb132caa2d1e603b75
Ochochejay <ochochejay@gmail.com>
2022-09-10 01:04:54 +0100

    Series of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Prints series of numbers to stdout.
+ * Return: 0.
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 8; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 1; d <= 9; d++)
+				{
+					putchar((a % 10) + '0');
+					putchar((b % 10) + '0');
+					putchar(' ');
+					putchar((c % 10) + '0');
+					putchar((d % 10) + '0');
+
+					if (!(a == 9 && b == 8 && c == 9 && d == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
