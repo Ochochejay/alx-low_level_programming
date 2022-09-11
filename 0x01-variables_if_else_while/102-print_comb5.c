fc0bb315bce13b581fd1f3414ba35d46a973e779
Ochochejay <ochochejay@gmail.com>
2022-09-11 15:04:52 +0100

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,32 +8,25 @@ int main(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = 0; b <= 8; b++)
+		for (b = (a + 1); b <= 99; b++)
 		{
-			for (c = 0; c <= 9; c++)
-			{
-				for (d = 1; d <= 9; d++)
-				{
-					putchar((a % 10) + '0');
-					putchar((b % 10) + '0');
-					putchar(' ');
-					putchar((c % 10) + '0');
-					putchar((d % 10) + '0');
+			putchar(a / 10 % 10 + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar(b / 10 % 10 + '0');
+			putchar((b % 10) + '0');
 
-					if (!(a == 9 && b == 8 && c == 9 && d == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			if (!(a == 98 && b == 99))
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
