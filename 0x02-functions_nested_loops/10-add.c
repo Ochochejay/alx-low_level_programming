e4ae175e792bab386ab49176396ae95469b2fd92
Ochochejay <ochochejay@gmail.com>
2022-09-13 23:31:52 +0100

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the sum.
+ * @a: Integer to add.
+ * @b: Integer to add.
+ * Return: sum.
+*/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - Prints out 9 timestable.
+ * Return: nothing.
+*/
+
+void times_table(void)
+{
+	int a, b, i;
+	
+	i = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (:
+			_putchar(i);
+
+			
+			if (a <  9)
+			{	
+				_putchar(',');
+				_putchar('  ');
+			}
+
