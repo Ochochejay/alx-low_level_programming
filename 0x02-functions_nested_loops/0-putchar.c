62eefc669af33c1c873fef23bdfff8902288921c
Ochochejay <ochochejay@gmail.com>
2022-09-13 13:24:15 +0100

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,30 +2,15 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * main - Entry point.
  * Description: Prints _putchar to stdout.
-=======
- * main - Displays a string to stdout.
->>>>>>> cb605090f25f03cc2013e3c8c1a075f437480bf9
  * Return: 0.
 */
 
-int main(void)
+int main()
 {
-<<<<<<< HEAD
 	_putchar();
-=======
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-
 	putchar('\n');
->>>>>>> cb605090f25f03cc2013e3c8c1a075f437480bf9
+
 	return (0);
 }
