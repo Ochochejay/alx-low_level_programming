97cf88b6184d8a7df14e9d79b8c7450b8dd82d04
ochochejay <ochochejay@gmail.com>
2022-09-14 23:11:53 -0700

    Print times-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,16 +9,33 @@ void print_times_table(int n)
 {
 	if (n >= 0 && n <= 15)
 	{
-		int a, b, c;
+		int a, b, c, d, e, f, g;
 
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
 				c = a * b;
+				d = c % 10;
+				e = c / 10;
+				f = e / 10;
+				g = e % 10;
 
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
+				if (c <= 9)
+					_putchar(d + '0');
+
+				else if (c > 9 && c <= 99)
+				{
+					_putchar(e + '0');
+					_putchar(d + '0');
+				}
+
+				else if (c >= 100)
+				{
+					_putchar(f + '0');
+					_putchar(g + '0');
+					_putchar(d + '0');
+				}
 
 				if (b < n)
 				{
