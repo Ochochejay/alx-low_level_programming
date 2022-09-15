728732e30fdea38dfea84d6080416ea64bbc58e6
Ochochejay <ochochejay@gmail.com>
2022-09-15 22:13:39 +0100

    Print Integers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: Integer to print.
+ * Return: void.
+ */
+
+void print_number(int n)
+{
+	int a, b, c, d, e, f;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	a = n % 10;
+	b = n / 10;
+	c = b / 10;
+	d = b % 10;
+	e = c / 10;
+	f = c % 10;
+
+	if (n < 10)
+		_putchar(a + '0');
+
+	else if (n >= 10 && n < 100)
+	{
+		_putchar(b + '0');
+		_putchar(a + '0');
+	}
+
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar(a + '0');
+	}
+
+	else
+	{
+		_putchar(e + '0');
+		_putchar(f + '0');
+		_putchar(d + '0');
+		_putchar(d + '0');
+	}
+}
