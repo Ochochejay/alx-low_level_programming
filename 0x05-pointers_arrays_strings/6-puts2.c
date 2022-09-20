af3ac69586c28482099fcabb451600516fd2eddb
Ochochejay <ochochejay@gmail.com>
2022-09-20 02:04:03 +0100

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,14 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
 	i = 0;
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i += 2;
-	}
+	while (*(str + i) != 0)
+		++i;
+
+	for (j = 0; j < i && str[j] != 0; i += 2)
+		_putchar(str[j]);
 
 	_putchar('\n');
 }
