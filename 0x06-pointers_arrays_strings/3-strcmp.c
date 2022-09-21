8ef24bc5190f7a0be7151388b67371b9a9bf2c55
Ochochejay <ochochejay@gmail.com>
2022-09-21 21:08:38 +0100

    Compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two string.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: count.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int count;
+
+
+	/*while (*(s1 + i) != '\0' && *(s2 + i) != '\0')*/
+	/*{*/
+		count = (*(s1 + 0) - *(s2 + 0));
+
+	/*	i++;*/
+	/*}*/
+
+	return (count);
+}
