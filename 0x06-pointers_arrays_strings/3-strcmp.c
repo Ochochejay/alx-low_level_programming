df23d515093f8e340bd1057acd1c642b374bcebc
Ochochejay <ochochejay@gmail.com>
2022-09-22 10:44:06 +0100

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,16 @@ int _strcmp(char *s1, char *s2)
 	int i, count;
 
 	i = count = 0;
-	while ((s1[i] != s2[i]) && (*(s1 + i) != '\0' && *(s2 + i) != '\0'))
+	
+	while ((*(s1 + i) != '\0' && *(s2 + i) != '\0'))
 	{
-		if (s1[i] > s2[i])
-			count += 5;
-		else if (s1[i] < s2[i])
-			count -= 5;
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] > s2[i])
+				count = -15;
+			else if (s1[i] < s2[i])
+				count = 15;
+		}
 
 		i++;
 	}
