014bd43f84989f44a30778b7042b27209306f612
Ochochejay <ochochejay@gmail.com>
2022-09-22 20:35:01 +0100

    copy strin

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0
-	while (dest[i] != 0)
+	i = 0;
+	while (src[i] != 0)
 	{
 		if (i < n)
 			dest[i] = src[i];
 		i++;
 	}
 
-
 	dest[i] = '\0';
+
 	return (dest);
 }
