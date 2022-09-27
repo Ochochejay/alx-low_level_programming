2b9b54f2340ddba64b83d370c05c1f02e45bf44e
Ochochejay <ochochejay@gmail.com>
2022-09-27 01:40:33 +0100

    Dereferencing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
