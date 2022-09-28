154bed79950cc138b6c38a65d0feef44a61f3591
Ochochejay <ochochejay@gmail.com>
2022-09-28 15:19:08 +0100

    Prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string.
+ * Return: void.
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+
+	if (*s)
+		_puts_recursion(s + 1);
+	else
+		_putchar('\n');
+}
