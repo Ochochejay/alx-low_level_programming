e9848ceae08ddac21b2c046d29b085e8c43562cf
Ochochejay <ochochejay@gmail.com>
2022-10-01 14:06:53 +0100

    Library functions

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Determines the absolute number of an integer.
+ * @n: Integer to test
+ * Return: the absolute value.
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+
+	return (n);
+}
