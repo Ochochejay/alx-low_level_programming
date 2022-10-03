44fa9e9f098cf733d30fcc71f67ded21eab636ac
Ochochejay <ochochejay@gmail.com>
2022-10-03 08:57:35 +0100

    cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on suucess else 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	int n[] = {25, 10, 5, 2, 1};
+	int i, j, ans, rem, coins;
+
+	coins = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	j = atoi(argv[1]);
+	while (j != 0)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			ans = j / n[i];
+			rem = j % n[i];
+			if (ans > 0)
+			{
+				coins += ans;
+				j = rem;
+				break;
+			}
+		}
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
