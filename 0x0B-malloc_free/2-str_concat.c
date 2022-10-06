d058644e8cbf8b688b552b65ecfed4dcecb27799
Ochochejay <ochochejay@gmail.com>
2022-10-07 00:12:03 +0100

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _strlen - calculates str length
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	if (s == NULL || *s == '\0')
+		return (0);
+
+	return (1 + _strlen(s + 1));
+}
+
+/**
  * str_concat - concatenates two strings.
  *
  * @s1: first string
@@ -11,28 +25,27 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
-	char tmp[100];
+	int len1, len2, i, j;
 	char *c;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-	for (i = 0; s1[i]; i++)
-		tmp[i] = s1[i];
-
-	for (j = 0; s2[j]; j++, i++)
-		tmp[i] = s2[j];
-
-	c = malloc(++i);
+	c = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (c == NULL)
 		return (NULL);
 
-	for (j = 0; j < i; j++)
-		c[j] = tmp[j];
+	for (i = 0; i < len1; i++)
+		c[i] = s1[i];
+
+	if (len2 == 0)
+		c[i] = '\0';
+
+	for (j = 0; j < len2; j++)
+		c[i + j] = s2[j];
+
+	c[i + j] = '\0';
 
 	return (c);
 }
