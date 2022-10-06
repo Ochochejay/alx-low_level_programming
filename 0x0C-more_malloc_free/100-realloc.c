618a45d51960b1fb436b648ac165a95c65c3d58f
Ochochejay <ochochejay@gmail.com>
2022-10-06 23:05:40 +0100

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates memory block
+ * @ptr: pointer to the memory
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ * Return: new pointer on success or NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *n_ptr;
+
+	if (new_size > old_size || ptr == NULL)
+	{
+		n_ptr = malloc(new_size);
+		if (n_ptr == NULL)
+			return (NULL);
+
+		free(ptr);
+		return (n_ptr);
+	}
+
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	else if (new_size == old_size)
+		return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /*MAIN_H*/
