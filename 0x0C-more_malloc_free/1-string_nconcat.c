f990bb1812a2d7813ca556fe83dd378326eb342c
Ochochejay <ochochejay@gmail.com>
2022-10-06 13:56:29 +0100

    Null terminator

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -45,8 +45,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
+
+	if (len2 == 0)
+		str[i] = '\0';
+
 	for (j = 0; j < len2; j++)
 		str[i + j] = s2[j];
+
+	str[i + j] = '\0';
 
 	return (str);
 }
