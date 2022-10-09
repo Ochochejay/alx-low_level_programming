4bb5fff552ce5b76e62c4e3951204e243c815834
Ochochejay <ochochejay@gmail.com>
2022-10-09 01:19:05 +0100

    splits string

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,7 +17,7 @@ char **strtow(char *str)
 
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0')
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			count++;
 	}
 	arr = malloc(sizeof(char *) * (count + 1));
