82c721c655b06e0c9b1836466bc9f4245e04a9dc
Ochochejay <ochochejay@gmail.com>
2022-10-09 01:16:34 +0100

    splits string

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * strtow - splits strings into a word
+ * @str: string
+ * Return: pointer or NULL
+ */
+
+char **strtow(char *str)
+{
+	char **arr;
+	int i, j, k, l, count;
+
+	count = k = j = 0;
+	if (!str || !*str)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0')
+			count++;
+	}
+	arr = malloc(sizeof(char *) * (count + 1));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			k++;
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+			{
+				arr[j] = malloc(sizeof(char) * (k + 1);
+				if (arr[j] == NULL)
+				{
+					for (l = 0; l < j; l++)
+						free(arr[l]);
+					free(arr);
+					return (NULL);
+				}
+				for (l = 0; l < k; l++)
+					arr[j][l] = str[i - j + 1 + l];
+				arr[j][l] = '\0';
+				j++;
+				k = 0;
+			}
+		}
+	}
+	arr[count] = '\0';
+	return (arr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -12,5 +12,6 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 int _length(int ac, char **av);
 int _strlen(char *s);
+char **strtow(char *str);
 
 #endif /*MAIN_H*/
