0c76313515fced458c41771250f5a59e2e1e96c2
Ochochejay <ochochejay@gmail.com>
2022-10-10 17:20:23 +0100

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* _DOG_H */
