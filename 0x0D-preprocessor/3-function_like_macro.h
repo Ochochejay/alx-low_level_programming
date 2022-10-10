1c95c0a5bae1e723ce96f3dc7e5ab171f6925051
Ochochejay <ochochejay@gmail.com>
2022-10-10 17:05:25 +0100

    Absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
-#endif /* ABS */
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x < 0) ? ((x) * (-1)) : (x))
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H */
