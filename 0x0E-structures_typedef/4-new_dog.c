bb040e666d1c16eec6332d95229236560de12174
Ochochejay <ochochejay@gmail.com>
2022-10-11 02:24:09 +0100

    new dog pointer

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: A pointer to a new dog or NULL on failure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+
+	n_dog = malloc(sizeof(dog_t));
+	if (n_dog == NULL)
+		return (NULL);
+
+	n_dog->name = name;
+	n_dog->age = age;
+	n_dog->owner = owner;
+
+	return (n_dog);
+}
