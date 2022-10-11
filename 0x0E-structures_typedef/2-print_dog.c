65a3bfc5a6fa172e6628e21ce28f8816b8adbcda
Ochochejay <ochochejay@gmail.com>
2022-10-11 02:06:09 +0100

    closing braces

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,9 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
-			printf("Name : %s\n", d->name);
+			printf("Name: %s\n", d->name);
 		else
-			printf("Name : (nil)\n");
+			printf("Name: (nil)\n");
 
 		printf("Age: %.6f\n", d->age);
 
