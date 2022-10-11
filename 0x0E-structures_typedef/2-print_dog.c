6f22fd24b15b4216d4d72fda9c8baa5e1b8f0146
Ochochejay <ochochejay@gmail.com>
2022-10-11 02:05:09 +0100

    rm spaces

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name : (nil)\n");
 
-		printf("Age : %.6f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
