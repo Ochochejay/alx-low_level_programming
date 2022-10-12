b99872c887291979c7fd17aa89bbe37224af6803
Ochochejay <ochochejay@gmail.com>
2022-10-12 15:34:26 +0100

    Executes another function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a func given as a parameter
+ * on each element of an arry
+ *
+ * @array: array
+ * @size: array size
+ * @action: pointer to a func
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
