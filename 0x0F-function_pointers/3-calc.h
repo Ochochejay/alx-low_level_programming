dc26a2c55bca25844e239fd9cf1ba5bec8330386
Ochochejay <ochochejay@gmail.com>
2022-10-12 23:59:32 +0100

    change header

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _3_CALC_H
+#define _3_CALC_H
 
 #include <stdio.h>
 #include <string.h>
@@ -17,12 +17,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int main(int ac, char *av[]);
+int (*get_op_func(char *s))(int, int);
 
-#endif /* CALC_H */
+#endif /* _3_CALC_H */
