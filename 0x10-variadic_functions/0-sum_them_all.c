bad7412adb842e47f293ec413cb9ccf199631ed3
Ochochejay <ochochejay@gmail.com>
2022-10-13 13:34:40 +0100

    debugarg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,8 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list arg;
-	int i, sum;
+	unsigned int i;
+	int sum;
 
 	if (n == 0)
 		return (0);
@@ -17,9 +18,9 @@ int sum_them_all(const unsigned int n, ...)
 	sum = 0;
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(arg[i], int);
+		sum += va_arg(arg, int);
 
-	va_end(arr);
+	va_end(arg);
 
 	return (sum);
 }
