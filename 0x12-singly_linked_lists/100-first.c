f67754c06f0b547497e0a9d629be40f3ad1bca84
Ochochejay <ochochejay@gmail.com>
2022-10-21 16:31:47 +0100

    Executes before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * exec_before main - executes before the main function.
+ * exec_before_main - executes before the main function.
  */
 
-void exec_before_main()
+void exec_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,7 +25,9 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void exec_before_main();
-void exec_before_main()__attribute__((constructor));
+void exec_before_main(void);
+
+/* This makes the func executable before main*/
+void exec_before_main(void)__attribute__((constructor));
 
 #endif /*LISTS_H*/
