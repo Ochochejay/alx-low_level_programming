13c9ddc99ad139b3ac4d0bf14dfd4d970c6e47c2
Ochochejay <ochochejay@gmail.com>
2022-10-21 14:26:39 +0100

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,14 +8,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *new;
+	list_t *new, *ptr;
 
-	while (head)
+	while (head != NULL)
 	{
-		new = head->next;
-		free(new->str);
-		free(new);
-		head = new;
+		free(head->str);
+		free(head);
+		head = head->next;
 	}
 
 }
