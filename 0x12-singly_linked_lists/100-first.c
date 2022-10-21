8bb9ba4c42bc5d7b8a37965e7541bc4cc6da94cd
Ochochejay <ochochejay@gmail.com>
2022-10-21 16:25:40 +0100

    Executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * exec_before main - executes before the main function.
+ */
+
+void exec_before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
