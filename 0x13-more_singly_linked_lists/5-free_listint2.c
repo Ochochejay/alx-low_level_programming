698f1c08a8f4678d89a4ec76065c41f562724033
Ochochejay <ochochejay@gmail.com>
2022-10-24 19:37:27 +0100

    Fix free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp = *head;
 
+	if (head == NULL)
+		return;
+
 	while (tmp)
 	{
 		*head = tmp->next;
 		free(tmp);
 		tmp = *head;
 	}
-	 head = NULL;
 }
